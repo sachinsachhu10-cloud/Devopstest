08e8e5dde3bbaca1773aacd20334f2dd88fb48b4
root <root@ip-10-0-29-241.ap-south-1.compute.internal>
2026-01-13 07:25:23 +0000

    Initial CI/CD project

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+// File: src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Application is running.");
+    }
+}
+
